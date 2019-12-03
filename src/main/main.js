d255f19bca32270259c83e41505e0f52c258020c
LiangYong <LiangDongXing.cn@outlook.com>
2019-12-03 09:41:24 +0800

    修改快捷键和全局托盘变量

--- src/main/main.js
+++ src/main/main.js
@@ -1,7 +1,8 @@
-const {app, BrowserWindow, Tray, Menu,globalShortcut } = require('electron');
+const {app, BrowserWindow, Tray, Menu, globalShortcut} = require('electron');
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
 let win;
+let tray;
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
@@ -44,7 +45,7 @@ function loadDocument() {
 }
 
 function createTray() {
-    let tray = new Tray("uninstallerIcon.ico");
+    tray = new Tray("uninstallerIcon.ico");
     const contextMenu = Menu.buildFromTemplate([
         {label: '退出', type: 'normal', role: 'quit'},
     ]);
@@ -56,11 +57,11 @@ function createTray() {
 }
 
 function createGlobalShortcut() {
-    globalShortcut.register('F2', () => {
+    globalShortcut.register('Control+Shift+Alt+T', () => {
         console.log(win.isMinimizable())
-        if (win.isFocused()){
+        if (win.isFocused()) {
             win.hide();
-        }else {
+        } else {
             win.show();
         }
     })
